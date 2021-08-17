577d6127f9c663e866b0b3e17f001167494aa40d
Uladzislau.Karol <Uladzislau.Karol@ithechart-group.com>
2021-08-17 18:16:49 +0300

    fixes: AsyncStorage issues

--- App.tsx
+++ App.tsx
@@ -3,11 +3,11 @@ import { StyleSheet, Text, View, Modal, Button } from 'react-native';
 
 import TabBar from './screens/tabbar';
 
-import { useHttp } from './hooks/http.hook';
+// import { useHttp } from './hooks/http.hook';
 import { useAuth } from './hooks/auth.hook';
 
 import store from './redux/store';
-import { Provider, useDispatch } from 'react-redux';
+import { Provider } from 'react-redux';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { ASYNC_STORAGE_AUTH } from './AsyncStorage';
 import { IUser } from './redux/initialState';
@@ -15,17 +15,19 @@ import { IUser } from './redux/initialState';
 const App: FC = () => {
 	const [isSignOutOpen, setIsSignOutOpen] = useState(false);
 
-	const { request, loading } = useHttp();
-	const { login, logout, isAuthenticated, user } = useAuth();
+	// const { request, loading } = useHttp();
+	const { login } = useAuth();
 
 	useEffect(() => {
 		async () => {
-			const data = await JSON.parse(AsyncStorage.getItem(ASYNC_STORAGE_AUTH) || '{}');
+			const storageData: string | null = await AsyncStorage.getItem(ASYNC_STORAGE_AUTH);
+			const data = JSON.parse(storageData || '{}');
 			const { token, userId } = data;
 			if (data && token && userId && typeof token === 'string' && typeof userId === 'string') {
 				login(data as IUser);
 			}
 		};
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
 	const signOutChangeHandler = () => {
@@ -54,7 +56,6 @@ const styles = StyleSheet.create({
 	},
 	signOutModalContainer: {
 		borderRadius: 10,
-		// position: 'absolute',
 		backgroundColor: 'pink',
 		marginTop: 350,
 	},
@@ -69,20 +70,14 @@ const styles = StyleSheet.create({
 		marginLeft: 10,
 		marginRight: 10,
 		marginBottom: 14,
-		// zIndex: 2,
 	},
 	tabBarContainer: {
 		position: 'absolute',
 		width: '100%',
-		// bottom: '100%',
 		top: '90%',
 		borderStyle: 'solid',
 		borderTopWidth: 1,
 		paddingTop: 7,
-
-		// justifyContent: 'flex-start',
-		// alignContent: 'flex-start',
-		// alignItems: 'flex-start',
 	},
 	tabBar: {
 		marginHorizontal: 15,
